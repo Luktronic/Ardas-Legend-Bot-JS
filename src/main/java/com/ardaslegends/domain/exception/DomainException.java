978f6022edf35f169ed834dc20d0c75670b48608
Luktronic <luktronic@gmx.at>
2025-01-29 21:14:39 +0100

    Added DomainException

---
+++ src/main/java/com/ardaslegends/domain/exception/DomainException.java
@@ -0,0 +1,9 @@
+package com.ardaslegends.domain.exception;
+
+/**
+ * @author <a href="mailto:luktronic@gmx.at">Luktronic</a>
+ */
+public class DomainException extends RuntimeException{
+
+	protected DomainException(String message) { super(message); }
+}
