78e2c08f46e68430766f5750ff5b3b437d8aca47
Luktronic <luktronic@gmx.at>
2025-01-29 21:17:35 +0100

    Added IllegalDomainActionException

---
+++ src/main/java/com/ardaslegends/domain/exception/IllegalDomainActionException.java
@@ -0,0 +1,11 @@
+package com.ardaslegends.domain.exception;
+
+/**
+ * @author <a href="mailto:luktronic@gmx.at">Luktronic</a>
+ */
+public class IllegalDomainActionException extends DomainException {
+
+	protected IllegalDomainActionException(String message) {
+		super(message);
+	}
+}
