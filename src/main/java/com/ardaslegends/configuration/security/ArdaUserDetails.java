de6199b07ed0fd6ffff4678bfcba0dff5ba2aff0
GroellerKarim <karim.jonas.groeller@gmail.com>
2025-02-14 00:06:56 +0100

    Fix ArdaUserDetails
    Signed-off-by: GroellerKarim <karim.jonas.groeller@gmail.com>

--- src/main/java/com/ardaslegends/configuration/security/ArdaUserDetails.java
+++ src/main/java/com/ardaslegends/configuration/security/ArdaUserDetails.java
@@ -37,7 +37,7 @@ public class ArdaUserDetails implements UserDetails {
 
     @Override
     public String getUsername() {
-        return user.getEmail();
+        return player.getIgn();
     }
 
     @Override
